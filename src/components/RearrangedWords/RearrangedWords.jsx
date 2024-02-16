704d9d63622ca45bfda17f9c5744d95c6341eec6
Enayet <enayetflweb@gamil.com>
2024-02-16 08:23:44 +0600

    download pdf set

--- src/components/RearrangedWords/RearrangedWords.jsx
+++ src/components/RearrangedWords/RearrangedWords.jsx
@@ -1,22 +1,60 @@
+'use client'
 import { globalContext } from '@/lib/GlobalContext/GlobalContext'
-import React, { useContext } from 'react'
+import jsPDF from 'jspdf';
+import React, { useContext, useEffect, useState } from 'react'
 
 const RearrangedWords = () => {
   const {wordCombinations} = useContext(globalContext)
   // console.log(wordCombinations)
+
+  const [shuffledWords, setShuffledWords] = useState([]);
+
+  useEffect(() => {
+    // Shuffle the words when the wordCombinations change
+    setShuffledWords(wordCombinations.sort(() => Math.random() - 0.5));
+  }, [wordCombinations]);
   const getPadding = (word) => {
     // Calculate the padding based on the length of the word
     return `${word.length+2}ch`;
   };
+
+  const downloadPDF = () => {
+    // Create a new PDF document
+    const doc = new jsPDF();
+    
+    // Set up initial y position for text
+    let yPos = 10;
+    
+    // Add each word combination to the PDF document
+  shuffledWords.forEach((word, index) => {
+    // Calculate the padding based on the length of the word
+    const padding = `${word.length + 2}ch`;
+    
+    // Add word and dash to the PDF document
+    doc.text(word + " -", 10, yPos);
+    
+    // Add blank space after the dash
+    doc.text("", 10 + word.length * 6, yPos);
+
+    yPos += 10; // Increase y position for the next word
+  });
+
+    // Save the PDF document
+    doc.save('word_combinations.pdf');
+  };
+
+
+
   return (
     <div>
       <h1 className='text-orange-900 text-5xl'>RearrangedWords</h1>
-      {wordCombinations.map((word, i) => (
+      {shuffledWords.map((word, i) => (
         <span key={i} className='text-orange-500'>
-          {word} -
-          <span style={{ paddingLeft: getPadding(word) }}>&nbsp;</span>
+          {word}- ,
+          {/* <span style={{ paddingLeft: getPadding(word) }}>&nbsp;</span> */}
         </span>
       ))}
+      <button onClick={downloadPDF}>Download as PDF</button>
     </div>
   )
 }
